52be44b60ea4f45cb2cccb7da7a06624c955ae1a
Austin Peel <austinpeel@gmail.com>
2023-04-12 13:55:48 +0200

    Move Utils to Components folder. Add CameraFacingObject script

---
+++ Assets/Components/Utils/CameraFacingObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFacingObject : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Update()
+    {
+        if (!mainCamera) mainCamera = Camera.main;
+
+        transform.forward = mainCamera.transform.forward;
+    }
+}
